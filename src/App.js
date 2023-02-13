1734fba4ab4464adbf2a9fe2525db3ac1468875e
KaraHowes <karahowes1@gmail.com>
2023-02-13 11:07:58 +0100

    a simple request

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import './App.css';
 function App() {
   return (
     <>
+    <h1>Why can't I change the email?</h1>
   <div>Let's go</div>
-  <div>let's check this email</div>
-  <div>let's check this email</div>
-  <div>let's check this email</div>
     </>
   );
 }
